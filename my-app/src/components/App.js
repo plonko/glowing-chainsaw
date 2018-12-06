461e0c498998fc7904293cd0181ea60f23b5397d
Seb <sebastian.thomas@aig.com>
2018-12-06 16:33:34 +0000

    Getting products

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,21 +1,47 @@
 import React, { Component } from "react";
+import { PRODUCT_API } from "../constants";
 import "../css/App.css";
 
 class Header extends Component {
   render() {
-    return <header className="header">Header</header>;
+    return <header className="header">Shop | Basket</header>;
   }
 }
 
 class Shop extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: []
+    };
+  }
+
+  componentDidMount() {
+    fetch(PRODUCT_API)
+      .then(resp => resp.json())
+      .then(products => this.setState({ products }));
+  }
+
   render() {
-    return <header className="shop">S</header>;
+    return (
+      <div className="shop">
+        {this.state.products.map(product => {
+          return (
+            <div>
+              <div>{product.name}</div>
+              <div>{product.description}</div>
+              <div>{product.price}</div>
+            </div>
+          );
+        })}
+      </div>
+    );
   }
 }
 
 class Basket extends Component {
   render() {
-    return <div className="basket">B</div>;
+    return <div className="basket">basket</div>;
   }
 }
 
---
+++ my-app/src/constants/index.js
@@ -0,0 +1,1 @@
+export const PRODUCT_API = "http://localhost:3010/products";
